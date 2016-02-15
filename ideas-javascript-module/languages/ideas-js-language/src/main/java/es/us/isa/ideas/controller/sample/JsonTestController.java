9386e146d02cee997c1aa99ac39e99b01b67d8e6
iremarmar <ire325@gmail.com>
2016-02-15 17:25:16 +0100

    Añadido el modulo de javascript

---
+++ ideas-javascript-module/languages/ideas-js-language/src/main/java/es/us/isa/ideas/controller/sample/JsonTestController.java
@@ -0,0 +1,12 @@
+package es.us.isa.ideas.controller.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.us.isa.ideas.module.controller.BaseTestModuleController;
+
+@Controller
+@RequestMapping("/tests")
+public class JsonTestController extends BaseTestModuleController {
+		
+}
